7dd7e1eef894f015af8745fbdc733b1c8b10eb64
Chris Soncrant <christopher.soncrant@gmail.com>
2021-10-15 21:48:03 -0700

    added styling and script for dropdown menu for mobile

---
+++ script.js
@@ -0,0 +1,15 @@
+const hamburger = document.querySelector(".hamburger");
+
+const navContainer = document.querySelector(".stupid-nav-container");
+
+let displayCheck = 0;
+
+hamburger.addEventListener('click', function() {
+    if (displayCheck === 0) {
+        navContainer.style.display = "block";
+        displayCheck = 1;
+    } else {
+        navContainer.style.display = "none";
+        displayCheck = 0;
+    }
+})
